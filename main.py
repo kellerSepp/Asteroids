6a396b1e03b730e684433bdac6b17f93cc2643f3
kellerSepp <se.kellermann@gmail.com>
2025-09-25 23:49:34 +0200

    added Constants and basic pygame screen + loop

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame
-#from database import connect_database, database_version
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
